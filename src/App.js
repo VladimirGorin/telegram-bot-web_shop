35ca04bfaf1e43f340564b0b3a2d1c5c6a930e9d
Владимир Горин <99133904+VladimirGorin@users.noreply.github.com>
2022-11-18 19:02:58 +0300

    Update
    Update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
-
-const tg = window.Telegram.WebApp;
-
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
 
 function App() {
+    const {onToggleButton, tg} = useTelegram();
 
-  const onClose = () => {
-    tg.close()
-  }
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
-  return (
-    <div className="App">
-        work font
-        <button onClick={onClose}>close</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
 }
 
 export default App;
