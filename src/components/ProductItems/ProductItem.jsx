9dc8a146ebbff439d66d720c8617373be2c51498
Владимир Горин <99133904+VladimirGorin@users.noreply.github.com>
2022-11-21 09:24:43 +0300

    asd
    asd

--- src/components/ProductItems/ProductItem.jsx
+++ src/components/ProductItems/ProductItem.jsx
@@ -28,6 +28,7 @@ function ProductItem(props) {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
+                    "Content-Security-Policy": "upgrade-insecure-requests"
                 },
                 body: JSON.stringify(data)
             })
